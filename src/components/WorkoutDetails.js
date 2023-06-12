26c45015140c49b74e7c3b092c09aff97f86024c
veronika-simic <veronikasimic21@gmail.com>
2023-06-12 11:39:01 +0200

    style workout details

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,11 +1,22 @@
-const WorkoutDetails = ({workout}) => {
-    return (<div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.load}</p>
-        <p><strong>Sets: </strong>{workout.sets}</p>
-        <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-    </div>) 
-}
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Sets: </strong>
+        {workout.sets}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
