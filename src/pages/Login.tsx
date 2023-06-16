06f4bf66c27a2d7def3327715736b099ae86ad6c
veronika-simic <veronikasimic21@gmail.com>
2023-06-16 08:01:09 +0200

    style forms

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,6 +14,7 @@ const Login = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)}  value={email}/>
             <label>Password</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+            <button>Login</button>
         </form>
     )
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -14,6 +14,7 @@ const SignUp = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)}  value={email}/>
             <label>Password</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+            <button>Sign up</button>
         </form>
     )
 }
