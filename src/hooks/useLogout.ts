314a4063883fc2b2a9da5f5273503b6d1830d306
veronika-simic <veronikasimic21@gmail.com>
2023-06-16 09:59:57 +0200

    assign workouts to user

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,12 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
-import { AuthActionType } from '../types/authAction';
-
+import { AuthActionType } from "../types/authAction";
+import { useWorkoutsContext } from "./useWorkoutsContext";
+import { ActionType } from "../types/workoutAction";
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = async () => {
     localStorage.removeItem("user");
-    dispatch({type: AuthActionType.Logout, payload: null})
+    dispatch({ type: AuthActionType.Logout, payload: null });
+    workoutsDispatch({ type: ActionType.SetWorkouts, payload: null });
   };
-  return {logout}
+  return { logout };
 };
