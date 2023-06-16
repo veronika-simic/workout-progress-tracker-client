91301e959a0718ed1e9a0e51995c1c73b186c1e3
veronika-simic <veronikasimic21@gmail.com>
2023-06-16 08:42:11 +0200

    login user

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log(email,password)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useLogin();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await login(email, password);
+    console.log(email, password);
+  };
+  return (
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Login</h3>
+      <label>Email:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
 
-    }
-    return (
-        <form className="login" onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <label>Email:</label>
-            <input type="email" onChange={(e) => setEmail(e.target.value)}  value={email}/>
-            <label>Password</label>
-            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-            <button>Login</button>
-        </form>
-    )
-}
-
-
-export default Login
+export default Login;
